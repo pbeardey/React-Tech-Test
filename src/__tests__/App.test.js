9262f10f9d3ef98b3c88444657ebd6fd047a0fe6
Paul Beard <pbeardey@gmail.com>
2022-06-07 20:55:10 +0100

    first commit

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,5 @@
+// src/__tests__/App.test.js
+
+import { render, screen } from '@testing-library/react';
+import App from '../components/App';
+
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+// src/components/App.js
+
+import React from "react";
+import '../styles/app.css';
+
+const App = () => {
+  return (
+    <div className="App">
+        <p>Hello World!</p>
+    </div>
+  );
+}
+
+export default App;
