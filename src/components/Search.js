bc40c3f960d2de37e0ac0ecec36073cc5bc6e770
Paul Beard <pbeardey@gmail.com>
2022-06-08 21:15:33 +0100

    add functionality to getImages query with the searchText

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,27 @@
-/* eslint-disable no-unused-vars */
 // src/components/Search.js
 import React, { useState } from "react";
+import getImages from "../requests/getImages";
 import "../styles/search.css";
 
 const Search = () => {
-  const [searchText, setSearchText] = useState()
+  const [searchText, setSearchText] = useState("")
+ 
   const handleInputChange = (event) => {
     setSearchText(event.target.value);
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getImages(searchText);
+  }
+
   return (
-    <>
-    <form className="search-form">
+    <div className="search">
+    <form className="search-form" onSubmit={handleSubmit}>
       <input className="search-input" type="text" onChange={handleInputChange}></input>  
       <button className="search-button" type="submit">Go!</button>
     </form>
-    </>
+    </div>
   )
 }
 
