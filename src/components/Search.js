fb7be1c530da8e82f046fb0400db0591c09414d4
Paul Beard <pbeardey@gmail.com>
2022-06-08 20:34:50 +0100

    add form, button and state

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,20 @@
+/* eslint-disable no-unused-vars */
 // src/components/Search.js
-import React from "react";
+import React, { useState } from "react";
 import "../styles/search.css";
 
 const Search = () => {
-  
+  const [searchText, setSearchText] = useState()
+  const handleInputChange = (event) => {
+    setSearchText(event.target.value);
+  }
+
   return (
     <>
-      <input className="search-input" type="text"></input>  
+    <form className="search-form">
+      <input className="search-input" type="text" onChange={handleInputChange}></input>  
+      <button className="search-button" type="submit">Go!</button>
+    </form>
     </>
   )
 }
