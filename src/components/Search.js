c9dd3773dbe0498e893d2c0ed717d6f6d40a512f
Paul Beard <pbeardey@gmail.com>
2022-06-12 16:46:55 +0100

    getImages to GET data from API; use the searchText as query

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,24 @@
 // src/__tests__/Search.test.js
-import React from "react";
+import React, {useState} from "react";
 import "../styles/search.css";
+import "../requests/getImages";
+import getImages from "../requests/getImages";
 
 const Search = () => {
+  const [searchText, setSearchText] = useState("");
+
+  const handleInputChange = (event) => {
+    setSearchText(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getImages(searchText);
+  }
 
   return (
-      <form className="search-form">
-        <input className="search-form__input"></input>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input className="search-form__input" onChange={handleInputChange}></input>
         <button className="search-form__button" type="submit">Go!</button>
       </form>
   )
---
+++ src/requests/getImages.js
@@ -0,0 +1,32 @@
+// src/requests/getImages.js
+
+import axios from "axios";
+
+const getImages = (query) => {
+   const API_ENDPOINT="https://images-api.nasa.gov/search?q=";
+  
+  if(!query) {
+    return Promise.resolve([]);
+  }
+  else {
+    return axios
+      .get(API_ENDPOINT+query)
+      .then((response)=>{
+        const imageResults = response.data.collection.items;
+        const parsedResults = imageResults.filter((e) => {return e.data[0].media_type==="image"})
+        const images = parsedResults.map((e) => {return {
+          nasaId: e.data[0].nasa_id,
+          href: e.links[0].href,
+          description: e.data[0].description
+        }});
+        console.log(images)
+      }
+      )
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+}
+
+export default getImages;
